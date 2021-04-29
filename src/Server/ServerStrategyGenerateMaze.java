deb605c4fef0c0e12429b5d4620f892a21e009b1
TamarTzuberi <tamar.tzuberi1@gmail.com>
2021-04-29 15:32:15 +0300

    class ServerStrategyGenerateMaze.java

--- src/Server/ServerStrategyGenerateMaze.java
+++ src/Server/ServerStrategyGenerateMaze.java
@@ -1,5 +1,30 @@
 package Server;
 
-public class ServerStrategyGenerateMaze {
+import IO.MyCompressorOutputStream;
+import algorithms.mazeGenerators.Maze;
+import algorithms.mazeGenerators.MyMazeGenerator;
 
+import java.io.*;
+
+public class ServerStrategyGenerateMaze implements IServerStrategy{
+
+    @Override
+    public void applyStrategy(InputStream inFromClient, OutputStream outToClient)  {
+
+        try {
+            ObjectInputStream fromClient= new ObjectInputStream(inFromClient);
+            ObjectOutputStream toClient= new ObjectOutputStream(outToClient);
+            int[] mazeDimens = (int[]) fromClient.readObject();
+            Maze m = new MyMazeGenerator().generate(mazeDimens[0],mazeDimens[1]);
+            ByteArrayOutputStream bArr = new ByteArrayOutputStream();
+            OutputStream out = new MyCompressorOutputStream(bArr);
+            out.write(m.toByteArray());
+            out.flush();
+            toClient.writeObject(bArr.toByteArray());
+            fromClient.close();
+            toClient.close();
+        } catch (Exception e) {
+        }
+
+    }
 }
